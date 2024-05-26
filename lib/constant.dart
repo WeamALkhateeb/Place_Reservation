e63db04514847c227cd602a7bb4ee8f46d14623d
WeamAlkhateeb <weamalkhateeb360@gmail.com>
2024-05-26 09:57:39 -0700

    creat directory

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/cupertino.dart';
 
-const Color firstBackColor = Color(0xFF093B80);
-const Color secondBackColor = Color(0xffEEF0FF);
-const Color thirdBackColor = Color(0xffD1D6DC);
+const Color firstBackColor = Color(0xFF142D55);
+const Color secondBackColor = Color(0xffeef0ff);
+const Color thirdBackColor = Color(0xffb7e3ff);
 
 
 const Color black = Color(0xff000000);
 const Color white = Color(0xffffffff);
-
